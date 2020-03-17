efa8453ea09e198120301d0810207653f071d225
fahriafif <61791206+fahriafif@users.noreply.github.com>
2020-03-17 11:22:05 +0700

    Add files via upload

---
+++ function.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package function;
+
+import model.modelprogram;
+
+/**
+ *
+ * @author Fahri Afif
+ */
+public class function {
+    public String returnName (modelprogram modelprogram){
+        return "Hello, your name must be " + modelprogram.getName();
+    }
+    
+}
